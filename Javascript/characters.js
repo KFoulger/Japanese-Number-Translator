f9f40acf11de6d2cbcd098811f9b711f44944930
KFoulger <kieranjf@sky.com>
2021-03-16 06:42:41 +0000

    Initial steps
    - Created a basic interface
    - Set up a "database" for the numbers with translations
    - Created a function for fetching translations
    - Created a function for breaking down bigger numbers

---
+++ Javascript/characters.js
@@ -0,0 +1,152 @@
+'use strict';
+
+function FindNumber(number){
+    let character;
+    let length = number.toString();
+    let numbers = [];
+    for(const char of length){
+        let num = number % 10;
+        numbers.unshift(num);
+        number = Math.trunc(number / 10);
+    }
+    for(const numbers of _characters){
+        if(numbers.romanNumeral === number){
+            character = numbers.hiragana;
+            break;
+        }
+    }
+    return character;
+}
+
+let _characters = [
+    {
+        "romanNumeral": 1,
+        "hiragana": "いち",
+        "kanji": "一"
+    },
+    {
+        "romanNumeral": 2,
+        "hiragana": "に",
+        "kanji": "二"
+    },
+    {
+        "romanNumeral": 3,
+        "hiragana": "さん",
+        "kanji": "三"
+    },
+    {
+        "romanNumeral": 4,
+        "hiragana": "よん",
+        "kanji": "四"
+    },
+    {
+        "romanNumeral": 5,
+        "hiragana": "ご",
+        "kanji": "五"
+    },
+    {
+        "romanNumeral": 6,
+        "hiragana": "ろく",
+        "kanji": "六"
+    },
+    {
+        "romanNumeral": 7,
+        "hiragana": "なな",
+        "kanji": "七"
+    },
+    {
+        "romanNumeral": 8,
+        "hiragana": "はち",
+        "kanji": "八"
+    },
+    {
+        "romanNumeral": 9,
+        "hiragana": "きゅう",
+        "kanji": "九"
+    },
+    {
+        "romanNumeral": 10,
+        "hiragana": "じゅう",
+        "kanji": "十"
+    },
+    {
+        "romanNumeral": 20,
+        "hiragana": "にじゅう",
+        "kanji": "二十"
+    },
+    {
+        "romanNumeral": 30,
+        "hiragana": "さんじゅう",
+        "kanji": "三十"
+    },
+    {
+        "romanNumeral": 40,
+        "hiragana": "よんじゅう",
+        "kanji": "四十"
+    },
+    {
+        "romanNumeral": 50,
+        "hiragana": "ごじゅう",
+        "kanji": "五十"
+    },
+    {
+        "romanNumeral": 60,
+        "hiragana": "ろくじゅう",
+        "kanji": "六十"
+    },
+    {
+        "romanNumeral": 70,
+        "hiragana": "ななじゅう",
+        "kanji": "七十"
+    },
+    {
+        "romanNumeral": 80,
+        "hiragana": "はち",
+        "kanji": "八十"
+    },
+    {
+        "romanNumeral": 90,
+        "hiragana": "きゅうじゅう",
+        "kanji": "九十"
+    },
+    {
+        "romanNumeral": 100,
+        "hiragana": "ひゃく",
+        "kanji": "百"
+    },
+    {
+        "romanNumeral": 300,
+        "hiragana": "さんびゃく",
+        "kanji": "三百"
+    },
+    {
+        "romanNumeral": 600,
+        "hiragana": "ろっぴゃく",
+        "kanji": "六百"
+    },
+    {
+        "romanNumeral": 800,
+        "hiragana": "はっぴゃく",
+        "kanji": "八百"
+    },
+    {
+        "romanNumeral": 1000,
+        "hiragana": "せん",
+        "kanji": "千"
+    },
+    {
+        "romanNumeral": 3000,
+        "hiragana": "さんぜん",
+        "kanji": "三千"
+    },
+    {
+        "romanNumeral": 8000,
+        "hiragana": "はっせん",
+        "kanji": "八千"
+    },
+    {
+        "romanNumeral": 10000,
+        "hiragana": "まん",
+        "kanji": "万"
+    }
+];
