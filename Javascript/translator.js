a382ec98bfe7f14e8bb77337999dd8bf60866790
KFoulger <kieranjf@sky.com>
2021-04-14 06:28:59 +0100

    html additions + changes
    - Renamed index.html to translate.html
    - Added a makeshift navigation bar
    - Created info.html which has all translations in tables with some additional info on how to translate
    - Cleaned up translator.js

--- Javascript/translator.js
+++ Javascript/translator.js
@@ -7,17 +7,3 @@ document.getElementById('translate').addEventListener('click', function(){
     document.getElementById('kanji').innerText = translated[1];
 })
 
-document.getElementById('random').addEventListener('click', function(){
-    window.location.href = "numberTest.html";
-})
-
-/*
-document.getElementById('genRandom').addEventListener('click', function(){
-    let min = document.getElementById('minRange').innerText;
-    let max = document.getElementById('maxRange').innerText;
-
-    let numberSet = getRandom(min, max);
-    document.getElementById('hira').innerText = numberSet[1];
-
-})
-*/
